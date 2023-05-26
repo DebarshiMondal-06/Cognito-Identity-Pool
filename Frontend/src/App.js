82639c7faeed6ddc2fa4e98668a738b8ba2dd21b
Debarshi Mondal <debopiku1122@gmail.com>
2023-05-26 13:11:40 +0530

    update react version

---
+++ Frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Component/Navbar';
+import { Context } from './Context/authContext';
+import CreateBook from './CreateBook';
+import Home from './Home';
+import './index.css';
+import View from './View';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Login from './Login';
+
+
+
+const App = () => {
+  return <Router>;
+    <Context>
+      <ToastContainer autoClose="1500" limit={1} />
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route path="/create" element={<CreateBook />} />
+        <Route path="/view/:id" element={<View />} />
+      </Routes>
+    </Context>
+  </Router>
+}
+
+export default App;
